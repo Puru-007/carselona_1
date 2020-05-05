d0a0391f5692a52976925085b18bc6f0962fb1be
Puru-007 <purveshpatil660@gmail.com>
2020-05-05 15:38:33 +0530

    LabeledRadio working

--- lib/myRadio1.dart
+++ lib/myRadio1.dart
@@ -6,48 +6,49 @@ import 'package:flutter_hex_color/flutter_hex_color.dart';
 import 'package:carselona1/radioCheck.dart';
 
 
-//class LabeledRadio extends StatelessWidget {
-//  const LabeledRadio({
-//    this.label,
-//    this.padding,
-//    this.groupValue,
-//    this.value,
-//    this.onChanged,
-//
-//
-//  });
-//
-//  final String label;
-//  final EdgeInsets padding;
-//  final bool groupValue;
-//  final bool value;
-//  final Function onChanged;
-//
-//
-//  @override
-//  Widget build(BuildContext context) {
-//    return InkWell(
-//      onTap: () {
-//        if (value != groupValue) onChanged(value);
-//      },
-//      child: Padding(
-//        padding: padding,
-//        child: Row(
-//          children: MyStatefulWidget1().map((titles) => Wrap (children: <Widget>[
-//            Radio<bool>(
-//              groupValue: groupValue,
-//              value: value,
-//              onChanged: (bool newValue) {
-//                onChanged(newValue);
-//              },
-//            ),
-//            Text(label),
-//          ],)).toList()
-//        ),
-//      ),
-//    );
-//  }
-//}
+class LabeledRadio extends StatelessWidget {
+  const LabeledRadio({
+    this.label,
+    this.padding,
+    this.groupValue,
+    this.value,
+    this.onChanged,
+
+
+  });
+
+  final String label;
+  final EdgeInsets padding;
+  final String groupValue;
+  final String value;
+  final Function onChanged;
+
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {
+        if (value != groupValue)
+          onChanged(value);
+      },
+      child: Padding(
+        padding: padding,
+        child: Row(
+          children: <Widget>[
+            Radio(
+              groupValue: groupValue,
+              value: value,
+              onChanged: (val) {
+                onChanged(val);
+              },
+            ),
+            Text(label),
+          ],
+        ),
+      ),
+    );
+  }
+}
 
 
 class MyStatefulWidget1 extends StatefulWidget {
@@ -79,19 +80,21 @@ class MyStatefulWidget1State extends State<MyStatefulWidget1> {
         children: radioTitle.map((titles) => Row(children: <Widget>[
 
 
-          Radio(
+          LabeledRadio(
+            label: "$titles",
             value: "$titles",
             groupValue: _radioValue,
+            padding: const EdgeInsets.symmetric(horizontal: 5.0),
             onChanged: (val) {
               setState(() {
                 _radioValue = val;
               });
             } ,
           ),
-          Text(
-            '$titles',
-
-          ),
+//          Text(
+//            '$titles',
+//
+//          ),
 // TODO-12: inside children array we will create a row(this row will have Radio and Text widget) using map() + toList() + Spread properties to create our radio button and Title text widget
           //done
           //TODO-13: Create Radio and Text Widget only once and set the text for Text Widget as "$title" and value, groupValue of Radio "$title".
